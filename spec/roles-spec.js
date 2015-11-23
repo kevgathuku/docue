801791d801075749fbd24124416c18e4dce3d095
Kevin Ndung'u <kevgathuku@gmail.com>
2015-11-23 17:10:24 +0300

    Add tests for getAllRoles function

--- spec/roles-spec.js
+++ spec/roles-spec.js
@@ -20,4 +20,27 @@ describe('Roles Spec', function() {
       done();
     });
   });
+
+  it('should return all roles when getAllRoles is called', function(done) {
+    roleController.all(function(err, roles) {
+      expect(err).toBeNull();
+      // The 3 seeded Roles should be returned
+      expect(roles.length).toBe(3);
+      done();
+    });
+  });
+
+  it('getAllRoles should return the correct roles', function(done) {
+    roleController.all(function(err, roles) {
+      // Make an array of the role titles
+      let allRoles = roles.map(function(role) {
+        return role.title;
+      });
+      // Assert that they contain the correct content
+      expect(allRoles).toContain('Admin');
+      expect(allRoles).toContain('SuperAdmin');
+      expect(allRoles).toContain('Sensei');
+      done();
+    });
+  });
 });
