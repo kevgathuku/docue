249a782d6420056d1974b8ac694ddab59baad16d
Kevin Ndung'u <kevgathuku@gmail.com>
2015-11-25 17:58:50 +0300

    Refactor user tests

--- spec/users.spec.js
+++ spec/users.spec.js
@@ -1,42 +1,88 @@
 describe('User Spec', function() {
   'use strict';
 
-  let helper = require('./helper');
-  let userController = require('../server/controllers/users');
+  const got = require('got');
+  const helper = require('./helper');
+  const userController = require('../server/controllers/users');
+  const baseUrl = 'http://localhost:3000/api/';
 
   beforeEach(function(done) {
     // Empty the DB then fill in some dummy data
+    this.usersRoute = baseUrl + 'users';
     helper.clearDb(function() {
-      helper.seedRoles(done);
+      helper.seedRoles(helper.seedUsers(function() {
+        done();
+      }));
     });
   });
 
   describe('User Creation', function() {
-    it('should create a unique user', function(done) {
-      userController.create('lastName', 'firstName', function(err, user) {
-        // A duplicate user should not be created
-        expect(user).toBeNull();
-        expect(err.name).toBe('ValidationError');
-        expect(err.message).toBe('User already exists');
-        done();
-      });
+    it('should create a user successfully', function(done) {
+      got.post(this.usersRoute, {
+          form: {
+            username: 'johnSnow',
+            firstname: 'John',
+            lastname: 'Snow',
+            email: 'snow@winterfell.org',
+            password: 'knfenfenfen'
+          }
+        })
+        .then(response => {
+          expect(response.statusCode).toBe(201);
+          expect(JSON.parse(response.body).username).toBe(
+            'johnSnow');
+          expect(JSON.parse(response.body).name.first).toBe(
+            'John');
+          expect(JSON.parse(response.body).name.last).toBe(
+            'Snow');
+        })
+        .catch(error => {
+          expect(error).toBeNull();
+        });
+      done();
     });
 
-    it('should create a user with both first and last names', function(done) {
-      userController.create('last', 'first', function(err, user) {
-        expect(user).not.toBeNull();
-        expect(user.name.last).toBe('last');
-        expect(user.name.first).toBe('first');
-        done();
-      });
+    it('should not create a duplicate user', function(done) {
+      // Try to create a duplicate user
+      got.post(this.usersRoute, {
+          form: {
+            username: 'jsnow',
+            firstname: 'John',
+            lastname: 'Snow',
+            email: 'jsnow@winterfell.org',
+            password: 'knfenfenfen'
+          }
+        })
+        .then(response => {
+          expect(response.statusCode).toBe(400);
+          expect(JSON.parse(response.body).error).toBe(
+            'User already exists');
+        })
+        .catch(error => {
+          expect(error).toBeNull();
+        });
+      done();
     });
 
-    it('a new user should have a role defined', function(done) {
-      userController.create('last', 'first', function(err, user) {
-        expect(user).not.toBeNull();
-        expect(user.role).not.toBeNull();
-        done();
-      });
+    it('should require user\'s role to be defined', function(done) {
+      got.post(this.usersRoute, {
+          form: {
+            username: 'johnSnow',
+            firstname: 'John',
+            lastname: 'Snow',
+            email: 'snow@winterfell.org',
+            password: 'knfenfenfen'
+          }
+        })
+        .then(response => {
+          expect(response.statusCode).toBe(400);
+          expect(JSON.parse(response.body).message).toBe(
+            'The user\'s role should be defined');
+        })
+        .catch(error => {
+          expect(error).toBeNull();
+        });
+      done();
     });
 
   });
@@ -44,11 +90,20 @@ describe('User Spec', function() {
   describe('getAllUsers function', function() {
 
     it('should return all users when called', function(done) {
-      userController.all(function(err, users) {
-        // The 3 seeded Users objects should be returned
-        expect(users.length).toBe(3);
-        done();
-      });
+      got(this.usersRoute)
+        .then(response => {
+          // Should return the 2 seeded users
+          expect(JSON.parse(response.body).length).toBe(2);
+          expect(JSON.parse(response.body[0]).username).toBe(
+            'jsnow');
+          expect(JSON.parse(response.body[1]).username).toBe(
+            'nstark');
+          console.log(response.body);
+        })
+        .catch(error => {
+          expect(error).toBeNull();
+        });
+      done();
     });
 
   });
