9ff1b1091f34a34a8d6877cc1650112fe8748aec
Kevin Ndung'u <kevgathuku@gmail.com>
2016-01-22 15:23:34 +0300

    [#112206745] Forward all not-found routes to frontend
    For all routes that are not matched in the server, forward the request to
    the frontend, which passes them through react-router

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,13 @@
   let express = require('express'),
     router = express.Router();
 
+  /* Delegate all unregistered routes to the frontend. */
+  router.get('*', function(req, res) {
+    res.sendFile('index.html', {
+      root: './public/'
+    });
+  });
+
   // The following routes now require a token to be accessed
   router.use('/api', require('./roles'));
   router.use('/api', require('./users'));
