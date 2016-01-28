c557463b157089360fd010daf3fac427cfe903e1
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-01-28 16:34:40 +0300

    [#112206745] Add session backend route
    - Add a route for checking whether a user is logged in or not.
    - This is used to determine whether to show the login/signup page or
    redirect to the homepage

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,17 +4,17 @@
   let express = require('express'),
     router = express.Router();
 
-  /* Delegate all unregistered routes to the frontend. */
+  // The following routes now require a token to be accessed
+  router.use('/api', require('./roles'));
+  router.use('/api', require('./users'));
+  router.use('/api', require('./documents'));
+
+  // Delegate all unregistered routes to the frontend
   router.get('*', function(req, res) {
     res.sendFile('index.html', {
       root: './public/'
     });
   });
-
-  // The following routes now require a token to be accessed
-  router.use('/api', require('./roles'));
-  router.use('/api', require('./users'));
-  router.use('/api', require('./documents'));
 
   module.exports = router;
 
