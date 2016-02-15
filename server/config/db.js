9e2e5f3cd9240b2887e1191438d01c0e05ccd41c
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-02-15 17:38:52 +0300

    [#113465613] Fix mongoose test url config
    Fix an issue with mongoose always connecting to the development DB even
    in test mode

--- server/config/db.js
+++ server/config/db.js
@@ -3,11 +3,12 @@
   'use strict';
 
   let mongoose = require('mongoose');
-  // MONGOLAB_URI is the MongoDB url config in Heroku
   if (process.env.NODE_ENV === 'test') {
     mongoose.connect(process.env.MONGO_TEST_URL);
+  } else {
+    // MONGOLAB_URI is the MongoDB url config in Heroku
+    mongoose.connect(process.env.MONGODB_URL || process.env.MONGOLAB_URI);
   }
-  mongoose.connect(process.env.MONGODB_URL || process.env.MONGOLAB_URI);
 
   let db = mongoose.connection;
 
