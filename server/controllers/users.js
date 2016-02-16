c9d86c16cd7ac559d4777a6505bed2ec6bf5fa35
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-02-16 17:48:27 +0300

    [#112659945] Implement Profile Update

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -121,7 +121,9 @@
         // Return the updated user object
         {
           new: true
-        }, (err, user) => {
+        })
+        .populate('ownerId')
+        .exec((err, user) => {
           if (!user) {
             return next(err);
           }
