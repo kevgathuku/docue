9a57a33b2403530cd6c4e76640649e992b3532cd
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-02-23 16:47:25 +0300

    [#114043561] User session tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,68 +1,73 @@
-module.exports = function(config) {
-  config.set({
+(() => {
+  'use strict';
 
-    browsers: ['PhantomJS'],
+  module.exports = function(config) {
 
-    singleRun: true,
+    config.set({
 
-    frameworks: ['mocha'],
+      browsers: ['PhantomJS'],
 
-    files: [
-      'tests.webpack.js'
-    ],
+      singleRun: true,
 
-    preprocessors: {
-      'tests.webpack.js': ['webpack', 'sourcemap']
-    },
+      frameworks: ['mocha'],
 
-    reporters: ['dots'],
+      files: [
+        'tests.webpack.js'
+      ],
 
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        noParse: [
-            /node_modules\/sinon/
-        ],
-        loaders: [{
-            test: /\.jsx?$/, // A regexp to test the require path. works for js or jsx
-            loader: 'babel', // The module to load. "babel" is short for "babel-loader"
-            exclude: /node_modules/,
-            query: {
-              presets: ['es2015', 'react', 'stage-0']
+      preprocessors: {
+        'tests.webpack.js': ['webpack', 'sourcemap']
+      },
+
+      reporters: ['dots'],
+
+      webpack: {
+        devtool: 'inline-source-map',
+        module: {
+          noParse: [
+              /node_modules\/sinon/
+          ],
+          loaders: [{
+              test: /\.jsx?$/, // A regexp to test the require path. works for js or jsx
+              loader: 'babel', // The module to load. "babel" is short for "babel-loader"
+              exclude: /node_modules/,
+              query: {
+                presets: ['es2015', 'react', 'stage-0']
+              }
+            }, {
+              test: /\.json$/,
+              loader: 'json'
+            }, {
+              test: /\.css$/,
+              exclude: /node_modules/,
+              loaders: ['style-loader', 'css-loader']
+            }, {
+              test: /\.(png|jpg|jpeg)$/,
+              exclude: /node_modules/,
+              loader: 'url-loader?limit=8192' // limit of 8kb
             }
-          }, {
-            test: /\.json$/,
-            loader: 'json'
-          }, {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            loaders: ['style-loader', 'css-loader']
-          }, {
-            test: /\.(png|jpg|jpeg)$/,
-            exclude: /node_modules/,
-            loader: 'url-loader?limit=8192' // limit of 8kb
-          }
 
-        ]
-      },
-      resolve: {
-        alias: {
-          sinon: 'sinon/pkg/sinon.js'
+          ]
         },
-        root: __dirname,
-        extensions: ['', '.js', '.jsx', '.json']
+        resolve: {
+          alias: {
+            sinon: 'sinon/pkg/sinon.js'
+          },
+          root: __dirname,
+          extensions: ['', '.js', '.jsx', '.json']
+        },
+        externals: {
+          'jsdom': 'window',
+          'cheerio': 'window',
+          'react/lib/ExecutionEnvironment': true,
+          'react/lib/ReactContext': true
+        }
       },
-      externals: {
-        'jsdom': 'window',
-        'cheerio': 'window',
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': true
+
+      webpackServer: {
+        noInfo: true
       }
-    },
 
-    webpackServer: {
-      noInfo: true
-    }
-
-  });
-};
+    });
+  };
+})();
