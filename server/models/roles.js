e68a9e1d806dfa91ecc5491d60b723f3146bc6d5
Salama AB <aksalj@aksalj.com>
2016-03-07 17:59:33 +0300

    let role model export titles and access level values; should allow them to change in future without affecting migration script

--- server/models/roles.js
+++ server/models/roles.js
@@ -3,20 +3,31 @@
 
   let mongoose = require('../config/db');
 
+  const ACCESS_LEVEL = {
+    'viewer': 0,
+    'staff': 1,
+    'admin': 2
+  };
+
+  const DEFAULT_ROLE = 'viewer';
+  const DEFAULT_ACCESS_LEVEL = ACCESS_LEVEL[DEFAULT_ROLE];
+
   let RoleSchema = mongoose.Schema({
     title: {
       type: String,
       unique: true,
       required: true,
-      default: 'viewer',
-      enum: ['viewer', 'admin', 'staff']
+      default: DEFAULT_ROLE,
+      enum: Object.keys(ACCESS_LEVEL)
     },
     // The accessLevel will be used to check for permissions
     accessLevel: {
       type: Number,
-      enum: [0, 1, 2]
+      default: DEFAULT_ACCESS_LEVEL,
+      enum: Object.keys(ACCESS_LEVEL).map( (title) => ACCESS_LEVEL[title] )
     }
   });
 
   module.exports = mongoose.model('Role', RoleSchema);
+  module.exports.ACCESS_LEVEL = Object.freeze(ACCESS_LEVEL);
 })();
