b72def053639240eb6b7dadb775315a01828d610
Kevin Ndung'u <kevgathuku@gmail.com>
2016-04-28 00:22:15 +0300

    Enable promises for the MongoDB Driver

--- server-src/config/db.js
+++ server-src/config/db.js
@@ -1,7 +1,7 @@
 /**
-* eslint no-console: 0
-* @flow
-*/
+ * eslint no-console: 0
+ * @flow
+ */
 (() => {
   'use strict';
 
@@ -11,16 +11,21 @@
   // Use bluebird for Mongoose promises
   mongoose.Promise = require('bluebird');
 
+  var options = {
+    promiseLibrary: require('bluebird')
+  };
+
   if (process.env.DB_1_PORT_27017_TCP_ADDR) {
     // Check if running in docker and set the DB address
     var address = process.env.DB_1_PORT_27017_TCP_ADDR;
     var port = process.env.DB_PORT_27017_TCP_PORT;
-    mongoose.connect(`mongodb://${address}:${port}/dms`);
+    mongoose.connect(`mongodb://${address}:${port}/dms`, options);
   } else if (process.env.NODE_ENV === 'test') {
-    mongoose.connect(process.env.MONGO_TEST_URL);
+    mongoose.connect(process.env.MONGO_TEST_URL, options);
   } else {
     // MONGOLAB_URI is the MongoDB url config in Heroku
-    mongoose.connect(process.env.MONGODB_URL || process.env.MONGOLAB_URI);
+    mongoose.connect(process.env.MONGODB_URL || process.env.MONGOLAB_URI,
+      options);
   }
 
   let db = mongoose.connection;
