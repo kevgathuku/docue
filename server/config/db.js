b96878c1b46ab178974f0dba13b01bd8ca31eee6
Kevin Ndung'u <kevgathuku@gmail.com>
2016-10-29 23:20:27 +0300

    Remove Docker MongoDB configuration

--- server/config/db.js
+++ server/config/db.js
@@ -18,12 +18,7 @@
     promiseLibrary: require('bluebird')
   };
 
-  if (process.env.DB_1_PORT_27017_TCP_ADDR) {
-    // Check if running in docker and set the DB address
-    var address = process.env.DB_1_PORT_27017_TCP_ADDR;
-    var port = process.env.DB_PORT_27017_TCP_PORT;
-    mongoose.connect(`mongodb://${address}:${port}/dms`, options);
-  } else if (process.env.NODE_ENV === 'test') {
+  if (process.env.NODE_ENV === 'test') {
     mongoose.connect(process.env.MONGO_TEST_URL, options);
   } else {
     // MONGOLAB_URI is the MongoDB url config in Heroku
