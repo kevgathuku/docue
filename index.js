c9a2f81bf55c25f3bcbbdd3d57e4c147c05d6fd8
Kevin Ndung'u <kevgathuku@gmail.com>
2016-10-31 13:51:52 +0300

    Use CORS npm package

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
   let express = require('express'),
     compression = require('compression'),
+    cors = require('cors'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     app = express(),
@@ -35,11 +36,7 @@
   app.use(bodyParser.json());
 
   // Enable CORS
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    next();
-  });
+  app.use(cors());
 
   let port = process.env.PORT || 8000; // set our port
 
