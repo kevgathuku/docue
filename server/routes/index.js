61dc35f0d11e45a5580d12abcf4ef7582fdb0e29
Kevin Ndung'u <kevgathuku@gmail.com>
2017-05-28 17:40:21 +0300

    Add stats route to return the count for each model

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,42 @@
-(() => {
-  'use strict';
+'use strict';
+const express = require('express'),
+  Promise = require('bluebird'),
+  router = express.Router(),
+  Documents = require('../models/documents'),
+  Roles = require('../models/roles'),
+  UsersController = require('../controllers/users'),
+  Users = require('../models/users');
 
-  let express = require('express'),
-    router = express.Router();
+router.use('/api', require('./roles'));
+router.use('/api', require('./users'));
+router.use('/api', require('./documents'));
 
-  // The following routes now require a token to be accessed
-  router.use('/api', require('./roles'));
-  router.use('/api', require('./users'));
-  router.use('/api', require('./documents'));
+const stats = (req, res) => {
+  // This action is available to admin roles only
+  if (req.decoded.role.title !== 'admin') {
+    res.status(403).json({
+      error: 'Unauthorized Access'
+    });
+  } else {
+    Promise.all([
+      Documents.count().exec(),
+      Users.count().exec(),
+      Roles.count().exec()
+    ])
+      .then(function(counts) {
+        var [docsCount, usersCount, rolesCount] = counts;
+        res.json({
+          documents: docsCount,
+          roles: rolesCount,
+          users: usersCount
+        });
+      })
+      .catch(err => {
+        res.next(err);
+      });
+  }
+};
 
-  module.exports = router;
+router.get('/api/stats', UsersController.authenticate, stats);
 
-})();
+module.exports = router;
