2507ed4f00d80b9e752129b316063d1bc71264f2
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-29 21:55:01 +0300

    Use  MongoMemoryServer for testing

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const isProduction = process.env.NODE_ENV === 'production';
 
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
 // load .env when not in production mode
 if (!isProduction) {
   require('dotenv').load();
@@ -11,14 +13,39 @@ mongoose.Promise = require('bluebird');
 
 // Enable Promises for the native MongoDB Driver
 const options = {
-  promiseLibrary: require('bluebird')
+  promiseLibrary: require('bluebird'),
+  useMongoClient: true,
 };
 
 if (process.env.NODE_ENV === 'test') {
-  mongoose.connect(process.env.MONGO_TEST_URL || process.env.MONGODB_URL, options);
+  const mongoServer = new MongoMemoryServer();
+  // mongoose.createConnection(process.env.MONGO_TEST_URL || process.env.MONGODB_URL, options);
+  mongoServer.getConnectionString().then(mongoUri => {
+    const mongooseOpts = {
+      // options for mongoose 4.11.3 and above
+      autoReconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000,
+      useMongoClient: true, // remove this line if you use mongoose 5 and above
+    };
+    console.log("mongoUri", mongoUri)
+    mongoose.connect(mongoUri, mongooseOpts);
+
+    mongoose.connection.on('error', e => {
+      if (e.message.code === 'ETIMEDOUT') {
+        console.log(e);
+        mongoose.connect(mongoUri, mongooseOpts);
+      }
+      console.log(e);
+    });
+
+    mongoose.connection.once('open', () => {
+      console.log(`TEST: MongoDB successfully connected to ${mongoUri}`);
+    });
+  });
 } else {
   // MONGOLAB_URI is the MongoDB url config in Heroku
-  mongoose.connect(
+  mongoose.createConnection(
     process.env.MONGODB_URL || process.env.MONGOLAB_URI,
     options
   );
