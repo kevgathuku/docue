a4a9bab0eb1c43caa78656b69179ca41306bc534
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-29 21:35:25 +0300

    Convert app test to use async/await

--- test/app.test.js
+++ test/app.test.js
@@ -2,13 +2,9 @@ import request from 'supertest';
 import test from 'ava';
 import { createTestApp } from '../index';
 
-test.cb('should raise 404 error if page is not found', t => {
-  t.plan(2);
-  request(createTestApp())
-    .get('/api/hows-your-father')
-    .end((err, res) => {
-      t.is(res.statusCode, 404);
-      t.is(res.body.error, 'Not Found');
-      t.end();
-    });
+test('should raise 404 error if page is not found', async t => {
+  const res = await request(createTestApp()).get('/api/hows-your-father');
+
+  t.is(res.statusCode, 404);
+  t.is(res.body.error, 'Not Found');
 });
